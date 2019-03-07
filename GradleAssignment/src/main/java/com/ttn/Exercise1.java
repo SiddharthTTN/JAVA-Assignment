f3a912a276e42c108c78f8d4c4a389a078e959bb
Siddharth Gupta <siddharth.gupta@tothenew.com>
2019-03-07 22:46:20 +0530

    Gradle Assignment Completed and Uploaded

---
+++ GradleAssignment/src/main/java/com/ttn/Exercise1.java
@@ -0,0 +1,12 @@
+package com.ttn;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        String str = new String("Siddharth");
+        System.out.println(StringUtils.equals(str,"Siddharth"));
+    }
+}
---
+++ GradleAssignment/src/main/java2/TestClass.java
@@ -0,0 +1,7 @@
+import org.apache.commons.lang3.StringUtils;
+
+public class TestClass {
+    public static void main(String[] args) {
+        System.out.println("This is in Class 2");
+    }
+}
